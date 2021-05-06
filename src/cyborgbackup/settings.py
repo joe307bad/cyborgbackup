8cd7564790eecc22633093dc62c8adcaf6f74acb
Joe Badaczewski <jbadaczewski@softwriters.com>
2021-05-05 23:47:34 -0400

    postgres port

--- src/cyborgbackup/settings.py
+++ src/cyborgbackup/settings.py
@@ -173,10 +173,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('POSTGRES_NAME', 'cyborgbackup'),
-        'USER': os.environ.get('POSTGRES_USER', 'user'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'pass'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'postgres'),
         'HOST': os.environ.get('POSTGRES_HOST', 'localhost'),
-        'PORT': os.environ.get('POSTGRES_PORT', 5432),
+        'PORT': os.environ.get('POSTGRES_PORT', 3079),
     }
 }
 
